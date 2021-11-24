8faac2824cf24618e5ba72a984a9bc697ed756aa
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2021-11-24 13:57:16 +0500

    img

--- filter.py
+++ filter.py
@@ -16,14 +16,18 @@ def get_average_brightness(block, size, gradation_step):
 
 
 def main():
-    image_file = Image.open(input("Введите имя файла, которое хотите конвертировать: "))
-    block_size = int(input("Введите размер блока: "))
-    gradations_count = int(input("Введите количество градаций серого: "))
-    image = np.array(image_file)
-    gradation_step = 255 // gradations_count
+    #image_file = Image.open(input("Введите имя файла, которое хотите конвертировать: "))
+    #block_size = int(input("Введите размер блока: "))
+    #gradations_count = int(input("Введите количество градаций серого: "))
+    #image = np.array(image_file)
+    image = np.array(Image.open('scale1200.jpg'))
+    #gradation_step = 255 // gradations_count
+    gradation_step = 255 // 50
 
-    res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
-    res.save(input("Введите имя файла, в которой хотите сохранить результат: "))
+    #res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
+    res = Image.fromarray(convert_image_to_mosaic(image, 10, gradation_step))
+    #res.save(input("Введите имя файла, в которой хотите сохранить результат: "))
+    res.save('res.jpg')
 
 
 if __name__ == '__main__':
